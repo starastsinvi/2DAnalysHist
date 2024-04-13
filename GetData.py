e776fd18265fa4fbe9602f3255ca2c312604b7f4
viktar <starastsinvi@ya.ru>
2024-04-14 01:15:44 +0300

    add png))

--- GetData.py
+++ GetData.py
@@ -6,12 +6,15 @@ import pandas as pd
 df = pd.read_csv('DATA_TXT_CLEAN/RUN010_dE2E5c.txt', sep='\s+', header=None, names=['x', 'y', 'z'])
 
 # Создаем гистограмму с 8000 бинов для каждой оси
-plt.hist2d(df['x'], df['y'], bins=6000, cmap='hot')
+plt.hist2d(df['x'], df['y'], bins=4000, cmap='hot')
 
 # Добавляем подписи осей и заголовок
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.title('Histogram')
+
+#plt.savefig('histogram_no_axes.png', bbox_inches='tight', dpi=600)
+
 
 # Отображаем гистограмму
 plt.show()
