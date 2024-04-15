92ac4fa091a45fdc07e43f91fd7f450a15913e52
starastsinvi <starastsinvi@ya.ru>
2024-04-15 11:51:52 +0300

    В скрипте GetData вывел исходную гистограмму и срез по бину по оси Y. Получая набор срезов можно попробовать сделать фит по гиперболе. Вопрос по поводу индентификации каналов внутри среза открыт. Вопрос по поводу калибровки - открыт.

--- GetData.py
+++ GetData.py
@@ -1,20 +1,42 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
+#
+# import numpy as np
 
-# Считываем новый файл
-df = pd.read_csv('DATA_TXT_CLEAN/RUN010_dE2E5c.txt', sep='\s+', header=None, names=['x', 'y', 'z'])
+#
+# Read the data from the file
+#
+# df = pd.read_csv('DATA_TXT_CLEAN/RUN010_dE2E5c.txt', sep=r'\s+', header=None, names=['x', 'y', 'z'])
 
-# Создаем гистограмму с 8000 бинов для каждой оси
-plt.hist2d(df['x'], df['y'], bins=4000, cmap='hot')
+# Read the original file
+df_original = pd.read_csv('DATA_TXT_CLEAN/RUN010_dE2E5c.txt', sep=r'\s+', header=None, names=['x', 'y', 'z'])
 
-# Добавляем подписи осей и заголовок
+# Create the original histogram
+plt.figure(figsize=(10, 10))
+plt.hist2d(df_original['x'], df_original['y'], bins=8000, cmap='Blues', alpha=1)
 plt.xlabel('X')
 plt.ylabel('Y')
-plt.title('Histogram')
+plt.title('Original Histogram')
+plt.colorbar()
+plt.tight_layout()
 
-#plt.savefig('histogram_no_axes.png', bbox_inches='tight', dpi=600)
+# Define the bin of interest
+bin_of_interest = 300
 
+# Filter the data to get the events in the bin of interest
+df_filtered = df_original[(df_original['x'] >= bin_of_interest) & (df_original['x'] < bin_of_interest + 10)]
 
-# Отображаем гистограмму
+# Plot the histogram of y values for the events in the bin of interest
+plt.figure(figsize=(10, 5))
+plt.hist(df_filtered['y'], bins=8000, alpha=0.5, color='r')
+plt.xlabel('Y')
+plt.ylabel('Number of events')
+plt.title('Histogram of Y values in bin {}'.format(bin_of_interest))
+plt.tight_layout()
+
+# Show the plots
 plt.show()
+
+#
+# plt.savefig('histogram_no_axes.png', bbox_inches='tight', dpi=600)
--- clean_txt.py
+++ clean_txt.py
@@ -1,16 +1,3 @@
-# import pandas as pd
-#
-# # Чтение файла с тремя столбцами, исключая первую строку
-# df = pd.read_csv('DATA_TXT/RUN010_dE2E5.txt', header=None, names=['bin1', 'bin2', 'count'], skiprows=1)
-#
-# # Удаление строк, где значение в третьем столбце равно 0
-# df = df[df['count'] != 0]
-#
-# # Создание нового файла для сохранения обновленных данных
-# with open('DATA_TXT_CLEAN/RUN010_dE2E5c.txt', 'w') as f:
-#     f.write('bin1\tbin2\tcount\n')  # Запись заголовка
-#     df.to_csv(f, index=False, header=False, sep='\t')  # Запись обновленных данных
-
 
 import pandas as pd
 
