4402ebe548621d903cc488f30e839735f725ce70
starastsinvi <starastsinvi@ya.ru>
2024-04-15 12:54:56 +0300

    В скрипте GetData вывел исходную гистограмму и срез по бину по оси Y. Получая набор срезов можно попробовать сделать фит по гиперболе. Вопрос по поводу индентификации каналов внутри среза открыт. Вопрос по поводу калибровки - открыт.

--- GetData.py
+++ GetData.py
@@ -13,7 +13,7 @@ import pandas as pd
 df_original = pd.read_csv('DATA_TXT_CLEAN/RUN010_dE2E5c.txt', sep=r'\s+', header=None, names=['x', 'y', 'z'])
 
 # Create the original histogram
-plt.figure(figsize=(10, 10))
+plt.figure(figsize=(8, 8))
 plt.hist2d(df_original['x'], df_original['y'], bins=8000, cmap='Blues', alpha=1)
 plt.xlabel('X')
 plt.ylabel('Y')
@@ -22,10 +22,10 @@ plt.colorbar()
 plt.tight_layout()
 
 # Define the bin of interest
-bin_of_interest = 300
+bin_of_interest = 500
 
 # Filter the data to get the events in the bin of interest
-df_filtered = df_original[(df_original['x'] >= bin_of_interest) & (df_original['x'] < bin_of_interest + 10)]
+df_filtered = df_original[(df_original['x'] >= bin_of_interest) & (df_original['x'] < bin_of_interest + 5)]
 
 # Plot the histogram of y values for the events in the bin of interest
 plt.figure(figsize=(10, 5))
